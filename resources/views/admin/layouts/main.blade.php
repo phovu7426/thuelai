792bc86970b6a7870f50f5c009a9088d2277fefe
hoangvm <vumanhhoang140799@gmail.com>
2025-07-05 16:18:53 +0700

    delete cache

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <!--begin::Head-->
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>AdminLTE v4 | @yield('title', 'Dashboard')</title>
+    <title>AdminLTE v4 | @yield('title', '')</title>
     <!--begin::Primary Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="title" content="AdminLTE v4 | Dashboard"/>
--- resources/views/admin/layouts/pathway.blade.php
+++ resources/views/admin/layouts/pathway.blade.php
@@ -5,7 +5,7 @@
         <!--begin::Row-->
         <div class="row">
             <div class="col-sm-6">
-                <h3 class="mb-0">@yield('page_title', 'Dashboard')</h3>
+                <h3 class="mb-0">@yield('page_title', '')</h3>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-end">
