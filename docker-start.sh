2ec3fa22e3b017e3989036bf44a850036f9e6580
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2025-07-07 23:07:16 +0700

    fix 9

--- docker-start.sh
+++ docker-start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ ! -f ".env" ]; then
+  echo "Tạo file .env mặc định"
+  cp .env.example .env
+fi
+
 # Tạo key nếu chưa có
 if ! grep -q "APP_KEY=" .env || [ -z "$(grep APP_KEY= .env | cut -d '=' -f2)" ]; then
   echo "Tạo APP_KEY..."
