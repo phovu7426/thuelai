a958cb7718692367da6620cf6d670c87766ec05c
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2025-08-25 16:43:21 +0700

    add css

--- resources/views/driver/layouts/main.blade.php
+++ resources/views/driver/layouts/main.blade.php
@@ -8,9 +8,9 @@
     <title>@yield('page_title', 'Dịch vụ lái xe thuê')</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link href="{{ asset('css/all.min.css') }}" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="{{ asset('css/driver.css') }}" rel="stylesheet">
 
