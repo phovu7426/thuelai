69c44a6604a831ddee29f1c36d9133d3fd6608f5
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2025-08-25 16:46:16 +0700

    add js

--- resources/views/driver/layouts/main.blade.php
+++ resources/views/driver/layouts/main.blade.php
@@ -272,7 +272,7 @@
     </footer>
 
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('bootstrap.bundle.min.js') }}"></script>
     <!-- Custom JS -->
     <script src="{{ asset('js/driver.js') }}"></script>
 
